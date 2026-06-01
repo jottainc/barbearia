5a6b6b64b2f62c15598da4793a13595bf429e6d8
jottainc <jnsilva495@gmail.com>
2026-06-01 19:34:33 +0000

    fix: corrige db.Column com C maiusculo no models.py

--- app/models.py
+++ app/models.py
@@ -4,16 +4,16 @@ from datetime import datetime
 
 class Agendamento (db.Model):  #// Cada agendamento da academia vai ser guardado aqui 
 
-    id = db.column(db.Integer, primary_key=True) #// Um numero unico para cada agendamento (o 'primaryKey' Garante que nunca vai repetir )
+    id = db.Column(db.Integer, primary_key=True) #// Um numero unico para cada agendamento (o 'primaryKey' Garante que nunca vai repetir )
 
-    nome_cliente = db.column(db.String(100), nullable=False) #// O nome de quem marcou (o 'nullabe' significa que esse campo é obrigaorio )
+    nome_cliente = db.Column(db.String(100), nullable=False) #// O nome de quem marcou (o 'nullabe' significa que esse campo é obrigaorio )
 
     
-    nome_barber = db.column(db.String(100), nullable=False)  #// ( Nome do barbeiro escolhido )
+    nome_barber = db.Column(db.String(100), nullable=False)  #// ( Nome do barbeiro escolhido )
 
     data_hora = db.Column (db.DateTime, nullable=False)  #// Dia e horarioo do agendamento 
 
     status = db.Column(db.String(20), default="Pendente") #// pendente, aceito ou recusado
 
-    criado_em = db.column(db.DateTime, default=datetime.utcnown) #// #A data do agendamento - (preenchido Automaticamente)
+    criado_em = db.Column(db.DateTime, default=datetime.utcnow) #// #A data do agendamento - (preenchido Automaticamente)
 
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,61 @@
 # //   Criação das rotas  // #
 
-from flask import Blueprint # Busca uma ferramenta do Flask chamada Blueprint. Pensa nela como uma "gaveta" onde guardamos rotas relacionadas.
+
+from flask import Blueprint, request, jsonify   #// " Requets = recepcionista. jsoniFY = tradutor "
+
+from app import db   
+
+from datetime import datetime
+
+from app.models import Agendamento
 
 main = Blueprint ("Main", __name__) #  Cria essa gaveta e dá o nome de main pra ela.
 
-@main.route ("/")  #// ("Quando alguem acessar o endereço "/" execute a função abaixo" ) - sso vai servir como a entrada inicial 
+@main.route("/agendamento", methods=["POST"])
+def criar_agendamento():
+    dados = request.get_json() #// Pega tudo que o usuario enviou e guarda em uma variavel (dados)
+    
+    novo = Agendamento(
+        nome_cliente=dados["nome_cliente"],  
+        nome_barber=dados["nome_barber"],
+        data_hora=datetime.strptime(dados["data_hora"], "%Y-%m-%d %H:%M"),
+    )
+    
 
-def index():  #// Função que vai ser usada quando alguem acessar a pagina inicial 
-    return "Seja bem vindo a barbearia !! Fique a vontade"
+    db.session.add(novo) #//Coloca o agendamento da fila para salvar     
+    db.session.commit()   #// "botão de salvar"
+
+    return jsonify ({
+        "mensagem": "Agendamento Criado com sucesso !!",
+        "id": novo.id}), 201 #// Mensagem de sucesso
+
+@main.route("/agendamentos", methods=["GET"])
+def listar_agendamentos():
+    agendamentos = Agendamento.query.all()  #// "Vai no banco e buscA TODOS OS AGENDAMENTOS"
+
+    resultado = []
+    for a in agendamentos:   #// PERCORRE CADA AGENDAMENTO CRIADO UM POR UM 
+        
+        resultado.append ({   #// PEGA CADA AGENDAMENTO E TRANSFORMA EM UM FORMATO LEGÍVEL PARA O USUARIO
+            "id": a.id,
+            "nome_cliente": a.nome_cliente,
+            "nome_barber": a.nome_barber,
+            "data_hora": str(a.data_hora),
+            "status": a.status
+        })
+
+    return jsonify(resultado), 200
+
+
+
+@main.route ("/cancelar_agendamentos/<int:id>/", methods=["DELETE"])
+def cancelar_agendamento(id):
+    agendamento_q = db.session.get(Agendamento,id)
+
+
+    if agendamento_q is None:
+        return jsonify ({"ERROR" : "AGENDAMENTO NÃO ENCONTRADO"}), 404
+    else:
+        db.session.delete(agendamento_q)
+        db.session.commit()
+        return jsonify ({"mensagem" : "Agendamento cancelado com sucesso"}), 200
